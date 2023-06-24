3e86c66af5a8ae017f869b5a2f2e511e2232eaa2
skyheros001 <32533832+skyheros001@users.noreply.github.com>
2023-06-24 11:03:57 +0800

    Update cimage.py

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -10,5 +10,6 @@ def convert_to_sd(img):
         scores = chunk["score"]
         logger.info(f"当前图片评分为 {scores}")
         #原来内容是 > 0.7，即高于0.7即为nsfw，压入内容，则any(shapes)=true后默认不换脸
+        #不建议删除评分检测，保留detect，可以自由切换使用场景
         shapes.append(chunk["score"] < -9999)
     return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
